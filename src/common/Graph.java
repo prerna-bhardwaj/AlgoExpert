99702606bf11ddebb1faa6a5d32e4499d05b61c9
Prerna Bhardwaj <prerna.us24@gmail.com>
2021-08-29 11:45:14 +0530

    All Easy Questions

---
+++ src/common/Graph.java
@@ -0,0 +1,60 @@
+package common;
+
+import java.util.ArrayList;
+
+public class Graph {
+	static int V;
+	static ArrayList<Integer>graph[];
+	
+	@SuppressWarnings("unchecked")
+	public Graph(int v) {
+		V = v;
+		graph = new ArrayList[V];
+		for(int i = 0; i < V; i++)
+			graph[i] = new ArrayList<Integer>();
+	}
+	
+	public void addDirectedEdge(int u, int v) {
+		graph[u].add(v);
+		graph[v].add(u);
+	}
+	
+	public void addUndirectedEdge(int u, int v) {
+		graph[u].add(v);
+	}
+
+	public static void printGraph(ArrayList<Integer>[]graph) {
+		for(int i = 0; i < graph.length; i++) {
+			System.out.println(i + " - " + graph[i]);	
+		}	
+		System.out.println();
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static ArrayList[] createDirectedGraph() {
+		Graph g = new Graph(7);
+		g.addDirectedEdge(0, 1);
+		g.addDirectedEdge(0, 3);
+		g.addDirectedEdge(1, 2);
+		g.addDirectedEdge(2, 3);
+		g.addDirectedEdge(2, 6);
+		g.addDirectedEdge(3, 4);
+		g.addDirectedEdge(4, 5);
+		g.addDirectedEdge(5, 6);
+	    return graph;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static ArrayList[] createUndirectedGraph() {
+		Graph g = new Graph(7);
+		g.addUndirectedEdge(0, 1);
+		g.addUndirectedEdge(0, 3);
+		g.addUndirectedEdge(1, 2);
+		g.addUndirectedEdge(2, 3);
+		g.addUndirectedEdge(2, 6);
+		g.addUndirectedEdge(3, 4);
+		g.addUndirectedEdge(4, 5);
+		g.addUndirectedEdge(5, 6);
+		return graph;
+	}
+}
---
+++ src/easy/DepthFirstSearch.java
@@ -0,0 +1,72 @@
+package easy;
+
+/*
+ * ADJACENCY LIST :
+ * For a directed graph, the sum of the sizes of the adjacency lists of all the nodes is E. 
+ * So, the time complexity in this case is O(V) + O(E) = O(V + E).
+ * For an undirected graph, each edge appears twice. Once in the adjacency list of either end of the edge. The time complexity for this case will be O(V) + O (2E) ~ O(V + E).
+ *	
+ * ADJACENCY MATRIX : 
+ * Time Complexity = O(V^2)
+ * 
+ * 4 types of edges in graph :
+ * 1. Forward edge - descendant is not part of dfs tree
+ * 2. Backward edge - ancestor is not part of dfs tree. Indicates cycle in graph
+ * 3. Tree edge - present in the dfs tree
+ * 4. Cross edge - no ancestor or descendant relationship
+ */
+import java.util.ArrayList;
+import java.util.Stack;
+
+import common.Graph;
+
+public class DepthFirstSearch {
+	int V;
+	ArrayList<Integer>graph[];
+	boolean[] visited;
+	
+	DepthFirstSearch() {
+		graph = Graph.createUndirectedGraph();
+		V = graph.length;
+		visited = new boolean[V];
+	}
+	
+	private void dfs(int node) {
+		System.out.print(node + " ");
+		visited[node] = true;
+		for(int ele : graph[node]) 
+			if(visited[ele] == false)
+				dfs(ele);		
+	}
+	
+	private void dfsIterative(int i) {
+		Stack<Integer>st = new Stack<>();
+		st.push(i);
+		visited = new boolean[V];
+		visited[i] = true;
+		
+		while(!st.isEmpty()) {
+			int node = st.pop();
+			System.out.print(node + " ");
+			
+			for(int ele : graph[node])
+				if(visited[ele] == false) {
+					st.push(ele);
+					visited[ele] = true;
+				}
+		}		
+	}
+	
+	public static void main(String[] args) {
+		DepthFirstSearch obj = new DepthFirstSearch();
+		Graph.printGraph(obj.graph);
+		
+		System.out.println("DFS traversal iterative starting from node 0 : ");
+		obj.dfs(0);	
+		
+		System.out.println("\n\nDFS traversal recursive starting from node 0 : ");
+		obj.dfsIterative(0);
+	}
+
+	
+}
